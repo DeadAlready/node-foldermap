f2f651b52ea68f47e23c089d161551df7488fb7f
Karl Dyyna <karl.dyyna@gmail.com>
2012-09-27 18:00:01 +0300

    Fix test script, bump version to 0.0.6

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 var mapper = require('../lib/map');
 
-var root = process.cwd() + path.sep;
+var root = process.cwd() + path.sep + 'test' + path.sep;
 
 var expected = {
   second:{
